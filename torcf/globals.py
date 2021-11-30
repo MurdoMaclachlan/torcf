9fc480cb868444e0a6284f71583abdd5b778499e
MurdoMaclachlan <47920497+MurdoMaclachlan@users.noreply.github.com>
2021-11-30 16:45:23 +0000

    Add globals and logger modules in preparation for restructure.

---
+++ torcf/globals.py
@@ -0,0 +1,51 @@
+"""
+    Copyright (C) 2021-present, Murdo B. Maclachlan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+    Contact me at murdomaclachlan@duck.com
+"""
+
+
+class GlobalVars:
+    def __init__(self):
+        self.CHECK_FOR_SUB = True
+        self.DEBUG = False
+        self.POSTS = []
+        # Remove found clones; recommended to keep disabled in the testing phase. Will
+        # always need to be disabled if you aren't a moderator, as you won't have this
+        # permission.
+        self.REMOVE = False
+        self.skip = False
+        self.VERBOSE = False
+        self.VERSION = "1.0.0-dev5-20211130"
+        self.WAIT = 30
+        if self.CHECK_FOR_SUB:
+            self.SUBREDDITS = input(
+                "Please enter the subreddits to search for, separated by spaces\n >> "
+            ).split(" ")
+
+
+class ToRPost:
+    def __init__(self: object, praw_obj: object, orig_sub: str, orig_link: str):
+        self.orig_link = orig_link
+        self.orig_sub = orig_sub
+        self.praw_obj = praw_obj
+        self.permalink = self.PRAW_OBJ.permalink
+
+    def delete(self: object):
+        self.praw_obj.delete()
+
+
+Globals = GlobalVars()
---
+++ torcf/logger.py
@@ -0,0 +1,94 @@
+"""
+    Copyright (C) 2021-present, Murdo B. Maclachlan
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+    Contact me at murdomaclachlan@duck.com
+"""
+
+from datetime import datetime
+from time import time
+from typing import List, NoReturn, Union
+
+
+class Logger:
+
+    def __init__(
+        self: object,
+        debug=0, error=1, fatal=1, info=1, warning=1
+    ) -> NoReturn:
+        self.__log = []
+        self.__scopes = {
+            "DEBUG":   debug,   # information for debugging the program
+            "ERROR":   error,   # errors the program can recover from
+            "FATAL":   fatal,   # errors that mean the program cannot continue
+            "INFO":    info,    # general information for the user
+            "WARNING": warning  # things that could cause errors later on
+        }
+
+    def get(self: object, mode="all") -> Union[List[str], str]:
+        """Returns item(s) in the log, either all items or the most recently added item.
+
+        Arguments:
+        - mode (single string): options are "all" and "recent".
+
+        Returns: a single log entry (string), list of log entries (string array), or
+                 an empty string on a failure.
+        """
+        if mode == "all":
+            return self.__log
+        elif mode == "recent":
+            return self.__log[len(self.__log)]
+        else:
+            self.new("Unknown mode passed to Logger.get().", "WARNING")
+            return ""
+
+    def get_time(self: object, time: int = time()) -> str:
+        """Gets the current time and parses it to a human-readable format.
+
+        Arguments:
+        - time (int): optional with 'time.time()' as default
+
+        Returns: a single date string in format 'YYYY-MM-DD HH:MM:SS'.
+        """
+        return datetime.fromtimestamp(time).strftime("%Y-%m-%d %H:%M:%S")
+
+    def new(
+            self: object,
+            message: str, scope: str, do_not_print: bool = False
+        ) -> bool:
+        """Initiates a new log entry and prints it to the console. Optionally, if
+        do_not_print is passed as True, it will only save the log and will not print
+        anything.
+
+        Arguments:
+        - messages (single string): the messaage to log.
+        - scope (single string): the scope of the message (e.g. debug, error, info).
+        - do_not_print (bool): optional, False by default.
+
+        Returns: boolean success status.
+        """
+        if self.__scopes[scope]:
+            self.__log.append(f"{[self.get_time()]} {scope}: {message}")
+            print(
+                f"{[self.get_time()]} {scope}: {message}"
+                if not do_not_print
+                else None
+            )
+            return True
+        return False
+
+
+global Log
+Log = Logger(debug=1)
